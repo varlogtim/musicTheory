4014ff0592337a7f2fb067c51500c17b223cbb5b
Tim Tucker <varlogtim@gmail.com>
2021-03-04 13:56:33 -0800

    Just testing SSH key

---
+++ src/musicTheory.js
@@ -0,0 +1,18 @@
+//
+// Thoughts:
+//
+// Goals:
+//  - Make understanding music patterns easier (starting with guitar)
+// 
+// How?:
+//  - Represent these patterns in an interactive web format.
+//  - Probably use colors to help with understand.
+//
+// Parts:
+//  - Need definitions for scales, chords, inetervals, etc... (model)
+//  - Need instrument specific displays (view)
+//  - .. yeah ... something like this
+//
+
+
+
